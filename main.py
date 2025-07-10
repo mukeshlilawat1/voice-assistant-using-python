e6843a69a3c77c950ae7db851b9e4257dfffe053
mukeshlilawat1 <mukeshkumarlilawat1@gmail.com>
2025-07-10 14:46:19 +0530

    add more features

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import speech_recognition as sr
 import pyttsx3
 import webbrowser
+import os
 # Removed: from wikipedia import languages (not used)
 import openai  # You can remove this if not used later
+import datetime
 
 def say(text):
     engine = pyttsx3.init()
@@ -58,8 +60,57 @@ if __name__ == '__main__':
                 webbrowser.open(music[1])
                 break
 
+            if "time" in query.lower():
+                # current_time = datetime.datetime.now()
+                # formatted_time = current_time.strftime("%H:%M:%S")
+                hour = datetime.datetime.now().strftime("%H")
+                minute = datetime.datetime.now().strftime("%M")
+                say(f"Sir Time is {hour} : {minute} Minutes Now ")
+                break
+
+            # Open Windows Applications
+            if "open notepad" in query.lower():
+                say("Opening Notepad")
+                os.system("start notepad")
+                break
+
+            if "open camera" in query.lower():
+                say("Opening Camera")
+                os.system("start microsoft.windows.camera:")
+                break
+
+
+            if "open calculator" in query.lower():
+                say("Opening Calculator")
+                os.system("start calc")
+                break
+
+            if "open paint" in query.lower():
+                say("Opening Paint")
+                os.system("start mspaint")
+                break
+
+            if "open command prompt" in query.lower() or "open cmd" in query.lower():
+                say("Opening Command Prompt")
+                os.system("start cmd")
+                break
+
+            if "open chrome" in query.lower():
+                say("Opening Google Chrome")
+                os.system("start chrome")
+                break
+
+        # if "open music" in query.lower() :
+        #     # music_url =
+        #    import subprocess, sys
+        #
+        #    opener = "open" if sys.platform == "darwin" else "xdg-open"
+        #      subprocess.call([opener, music_url])
+
+
+
         if "stop" in query.lower():
              say("Goodbye Sir, have a nice day")
              break
 
-     
+
